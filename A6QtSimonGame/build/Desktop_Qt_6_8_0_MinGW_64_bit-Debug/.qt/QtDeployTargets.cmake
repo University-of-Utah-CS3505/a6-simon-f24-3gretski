e05ed5740dfcd59082c42bb4d57247c88ace1159
3gretski <3gretski@gmail.com>
2024-10-18 17:07:17 -0600

    Added Initial files including cmake etc

---
+++ A6QtSimonGame/build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_A6QtSimonGame_FILE C:/Users/3gret/source/repos/uofu-cs3500-spring24/a6-simon-f24-3gretski/A6QtSimonGame/build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/A6QtSimonGame.exe)
+set(__QT_DEPLOY_TARGET_A6QtSimonGame_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_A6QtSimonGame_RUNTIME_DLLS C:/Qt/6.8.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Core.dll)
