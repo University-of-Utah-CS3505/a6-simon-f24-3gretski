75715590d85eff4b65b618553a86f5ddf717389d
3gretski <144959909+3gretski@users.noreply.github.com>
2024-10-21 12:48:17 -0600

    Added old repo files

---
+++ model.cpp
@@ -0,0 +1,30 @@
+#include "model.h"
+#include <iostream>
+#include <vector>
+
+
+std::vector<int> sequence = {};
+
+model::model(QObject *parent) : QObject(parent)
+{
+
+}
+
+void model::startGame() {
+    std::cout << "clicked start" << std::endl;
+    //set enabled to true
+
+}
+
+void model::redButtonPressed() {
+
+}
+
+void model::blueButtonPressed() {
+
+}
+
+// Helper to Take in parameter representing which button color pressed
+void model::flashSequence() {
+
+}
